54fea23b9ec1da720d485480b83b7b3f34fc53d0
mib <jajan151@seznam.cz>
2023-01-02 01:13:23 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+min_value = int(input("Zadej spodní hranici: "))
+max_value = int(input("Zadej horní hranici: "))
+
+hadej = random.randint(min_value, max_value)
+nespravna_cisla = []
+pocet_pokusu = 1
+
+while True:
+	tip = int(input("Tipni číslo: "))
+	if tip in nespravna_cisla:
+		pocet_pokusu += 1
+		print("Zadal jsi už jednou zadané číslo!")
+	else:
+		if tip == hadej:
+			print(f"Uhádl jsi na {pocet_pokusu}. pokus.")
+			break
+
+		elif tip > hadej:
+			nespravna_cisla.append(tip)
+			print("Hadane cislo je mensi")
+			pocet_pokusu += 1
+
+		elif tip < hadej:
+			nespravna_cisla.append(tip)
+			print("Hadane cislo je větší")
+			pocet_pokusu += 1
+
+		print(f"Tebou zadaná čísla: {nespravna_cisla}")
